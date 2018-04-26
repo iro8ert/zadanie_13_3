e0e265334cdcbed68412f68adbda73d7cc221289
Robert Nowaczyk <iro8ert@o2.pl>
2018-04-26 21:24:43 +0200

    program.js

--- program.js
+++ program.js
@@ -3,14 +3,17 @@ process.stdin.on('readable', function(){
 	var input = process.stdin.read();
 	if(input !== null) {
 		var instruction = input.toString().trim();
-	 	switch (instruction)  {
+	 	switch(instruction)  {
 			case '/exit':
 				process.stdout.write('Quittin app!\n');
 				process.exit();
-		 	break;
+		 		break;
+	 		case '/sayhello':
+	 			process.stdout.write('hello!\n');
+	 			break;
 			default:
 				process.stderr.write('Wrong instruction!\n');
-		}
+		};
 	}
 });
 
