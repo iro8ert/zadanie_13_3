82a7503719cf56ce1f3bcc671078f33cb181c3e4
Robert Nowaczyk <iro8ert@o2.pl>
2018-04-26 17:04:21 +0200

    change program.js

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function(){
+	var input = process.stdin.read();
+	if(input !== null) {
+		process.stdout.write(input);
+	}
+});
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+	 	switch  {
+			case (instruction === '/exit'):
+				process.stdout.write('Quittin app!\n');
+				process.exit();
+		 	break;
+			case (instruction):
+				process.stderr.write('Wrong instruction!\n');
+		}
+	}
+});
